b36e8b60e5fd8b768572d434665df321f38bbb17
Edniz Silva <87082825+EdiSil@users.noreply.github.com>
2024-11-30 03:18:11 -0300

    Update Home.py
    segundo

--- Home.py
+++ Home.py
@@ -1,24 +1,23 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title = "PISI3 - BSI - UFRPE por Gabriel Alves",
+    page_title = "PISI3 - BSI - UFRPE por Edniz Silva",
     layout = "wide",
     menu_items = {
-        'About': '''Este sistema foi desenvolvido pelo prof Gabriel Alves para fins didáticos, para a disciplina de 
-        Projeto Interdisciplinar para Sistemas de Informação 3 (PISI3) do 3° período do curso de Bacharelado em Sistemas de Informação
-        (BSI) da Universidade Federal Rural de Pernambuco (UFRPE).
-        Dúvidas? gabriel.alves@ufrpe.br
-        Acesse: bsi.ufrpe.br
+        'About': '''Este projeto foi desenvolvido com fins acadêmicos, utilizando técnicas de Machine Learning (ML) e Knowledge Discovery 
+        in Databases (KDD), com o objetivo de extrair conhecimento valioso a partir de dados brutos e apoiar decisões de negócios, 
+        como ajustar preços de venda ou identificar modelos mais populares com base nas características.
         '''
     }
 )
 
 st.markdown(f'''
-    <h1>Sistema Didático para PISI3</h1>
+    <h1>UFRPE PISI3</h1>
     <br>
-    Este projeto tem o objetivo de prover vários exemplos úteis para os projetos que serão desenvolvidos na disicplina de 
-    Projeto Interdisciplinar para Sistemas de Informação 3 (PISI3) do 3° período do curso de Bacharelado em Sistemas de Informação
-    (BSI) da Sede da Universidade Federal Rural de Pernambuco (UFRPE).
+Este projeto foi desenvolvido com fins acadêmicos, utilizando técnicas de Machine Learning (ML) e Knowledge Discovery 
+in Databases (KDD), com o objetivo de extrair conhecimento valioso a partir de dados brutos e apoiar decisões de negócios, 
+como ajustar preços de venda ou identificar modelos mais populares com base nas características. do curso de Bacharelado em Sistemas de
+Informação (BSI) da Sede da Universidade Federal Rural de Pernambuco (UFRPE).
     <br>
     Alguns dos exemplos são:
     <ul>
@@ -29,7 +28,5 @@ st.markdown(f'''
             <li>Visualização de dados.</li>
             <li>Aprendizado de Máquina: Agrupamento e Classificação.</li>
     </ul>
-    Classroom: <a href="https://classroom.google.com/c/NjExNTAzOTU4MDQy?cjc=7qgaz7u">https://classroom.google.com/c/NjExNTAzOTU4MDQy?cjc=7qgaz7u</a><br>
-    Contato: gabriel.alves@ufrpe.br<br>
-    Acesse: <a href="bsi.ufrpe.br">bsi.ufrpe.br</a>
+    Contato: ednizsilva@ufrpe.br<br>    
 ''', unsafe_allow_html=True)
