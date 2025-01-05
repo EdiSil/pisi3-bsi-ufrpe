44ebddba216baf9f65880fbc9eb62e60dfef58b0
Edniz Silva <87082825+EdiSil@users.noreply.github.com>
2025-01-05 03:06:49 -0300

    Update 2_ Mais Analises.py
    39

--- pages/2_ Mais Analises.py
+++ pages/2_ Mais Analises.py
@@ -165,6 +165,6 @@ class CarAnalysisApp:
         self.show_treemap_brand_model()
 
 if __name__ == "__main__":
-    data_path = "Datas/1_Cars_dataset_processado.csv"
+    data_path = "Datas/1_Cars_processado.csv"
     app = CarAnalysisApp(data_path)
     app.run_app()
