d882967057a9495408a023dd4126d0939b544c7f
Edniz Silva <87082825+EdiSil@users.noreply.github.com>
2025-01-26 17:26:59 -0300

    Update 3_Mais Analises.py
    09

--- pages/3_Mais Analises.py
+++ pages/3_Mais Analises.py
@@ -115,7 +115,7 @@ def main():
         # Painel de controle para o usuário
         st.sidebar.header("CONFIGURAÇÕES")
         max_clusters = st.sidebar.slider("NÚMERO MÁXIMO DE CLUSTERS (MÉTODO DO COTOVELO):", 2, 12, 12)
-        num_clusters = st.sidebar.slider("NÚMERO DE CLUSTERS (ANÁLISE):", 1, 5, 5)
+        num_clusters = st.sidebar.slider("NÚMERO DE CLUSTERS (MATRIZ):", 1, 5, 5)
 
         # Exibir os gráficos
         st.subheader("MÉTODO DO COTOVELO")
