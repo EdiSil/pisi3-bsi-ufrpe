2c382f1d7541deb3d3b7ae9adfd686418100631a
Edniz Silva <87082825+EdiSil@users.noreply.github.com>
2025-03-09 19:07:03 -0300

    Update Clusterização.py
    upd

--- pages/Clusterização.py
+++ pages/Clusterização.py
@@ -206,7 +206,7 @@ def main():
         df = pd.read_csv(file_path)
         
         if 'preco' in df.columns:
-            df['preco'] = df['preco'] / 1000
+            df['preco'] = df['preco'] * 6
         
         st.session_state['data'] = df
         st.success("DADOS CARREGADOS COM SUCESSO!")
