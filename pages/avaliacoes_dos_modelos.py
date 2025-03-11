3a713f4bedd0d26f1f2fe7452daa263c8899f71b
Edniz Silva <87082825+EdiSil@users.noreply.github.com>
2025-03-10 22:40:58 -0300

    Update avaliacoes_dos_modelos.py
    up

--- pages/avaliacoes_dos_modelos.py
+++ pages/avaliacoes_dos_modelos.py
@@ -265,8 +265,8 @@ def main():
         todas_caracteristicas = avaliador.dados.columns.tolist()
         caracteristicas_selecionadas = st.sidebar.multiselect(
             "Selecione as Características",
-            todas_caracteristicas,
-            default=['quilometragem', 'Car Age', 'Cluster']
+            [col for col in todas_caracteristicas if col not in ['car_documents', 'Predicted Price', 'Características']],
+            default=['modelo', 'ano', 'preco', 'combustivel', 'tipo', 'transmissão', 'year_range', "km's driven_range"]
         )
 
         if caracteristicas_selecionadas:
